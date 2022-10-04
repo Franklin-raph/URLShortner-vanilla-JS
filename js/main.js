ef8ff0a2e87dccec1e871d929ceccc3b0c76c23a
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-10-04 17:52:53 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// async function getLink(){
+//     const res = await fetch(" https://api.shrtco.de/v2/shorten?url=franklinraphaelportfolio.netlify.app")
+//     const data = await res.json();
+//     console.log(data)
+// }
+
+// getLink()
